0c14c77f31c088be991616fe3ccca38daab520f4
peterchase <peter.chase@grantadesign.com>
2014-03-13 15:44:52 +0000

    It's all static

--- SillyCode/SillyCode.cs
+++ SillyCode/SillyCode.cs
@@ -4,34 +4,34 @@ using System.Linq;
 
 namespace SillyCode
 {
-    public class SillyCode
+    public static class SillyCode
     {
-        public string GetOrCreateValue(IDictionary<string, string> map, string key, Func<string> createValue)
+        public static string GetOrCreateValue(IDictionary<string, string> map, string key, Func<string> createValue)
         {
             return map.ContainsKey(key) ? map[key] : map[key] = createValue();
         }
 
-        public bool IsEmpty<T>(IEnumerable<T> collection)
+        public static bool IsEmpty<T>(IEnumerable<T> collection)
         {
             return collection.Count() == 0;
         }
 
-        public bool HaveSameSum(IEnumerable<double> firstValues, IEnumerable<double> secondValues)
+        public static bool HaveSameSum(IEnumerable<double> firstValues, IEnumerable<double> secondValues)
         {
             return firstValues.Sum() == secondValues.Sum();
         }
 
-        public IEnumerable<int> CountFrequencies<T>(IEnumerable<T> sourceCollection, IEnumerable<Predicate<T>> predicates)
+        public static IEnumerable<int> CountFrequencies<T>(IEnumerable<T> sourceCollection, IEnumerable<Predicate<T>> predicates)
         {
             return predicates.Select(p => sourceCollection.Count(v => p(v))).ToList();
         }
 
-        public int GetHashCode(ushort value)
+        public static int GetHashCode(ushort value)
         {
             return value >> 16 + 12;
         }
 
-        public bool ReferenceSameObjects<TKey, TValue>(IEnumerable<TKey> keys1, IEnumerable<TKey> keys2, IDataSource<TKey, TValue> remoteDataSource)
+        public static bool ReferenceSameObjects<TKey, TValue>(IEnumerable<TKey> keys1, IEnumerable<TKey> keys2, IDataSource<TKey, TValue> remoteDataSource)
             where TValue : class
         {
             var valuePairs = keys1.SelectMany(key1 
